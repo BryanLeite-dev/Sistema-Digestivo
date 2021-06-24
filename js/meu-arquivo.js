a5c0849a6e390545a5c6c678fab13a90e50b1d16
BryanLeite-dev <bryan.leite@etc.sp.gov.br>
2021-06-24 11:28:11 -0300

    Inclusão do Projeto

---
+++ js/meu-arquivo.js
@@ -0,0 +1,204 @@
+function abrirJogo (){
+	open("file:///C:/Users/Pichau/OneDrive%20-%20Etec%20Centro%20Paula%20Souza/Desktop/Projetos/Sistema%20Digest%C3%B3rio_html/Quiz.html");
+	
+}
+function abrirComeco(){
+	open("file:///C:/Users/Pichau/OneDrive%20-%20Etec%20Centro%20Paula%20Souza/Desktop/Projetos/Sistema%20Digest%C3%B3rio_html/Inicio.html")
+}
+function abrirErrado (){
+	open("file:///C:/Users/Pichau/OneDrive%20-%20Etec%20Centro%20Paula%20Souza/Desktop/Projetos/Sistema%20Digest%C3%B3rio_html/Quiz.html");
+	
+}
+function abrirJogar(){
+	open("file:///C:/Users/Pichau/OneDrive%20-%20Etec%20Centro%20Paula%20Souza/Desktop/Projetos/Sistema%20Digest%C3%B3rio_html/P1.html");
+}
+
+function abrirSistema (){
+	open("file:///C:/Users/Pichau/OneDrive%20-%20Etec%20Centro%20Paula%20Souza/Desktop/Projetos/Sistema%20Digest%C3%B3rio_html/Sistema.html");
+}
+function abrirInicio (){
+	open("file:///C:/Users/Pichau/OneDrive%20-%20Etec%20Centro%20Paula%20Souza/Desktop/Projetos/Sistema%20Digest%C3%B3rio_html/Quiz.html");
+	close("file:///C:/Users/Pichau/OneDrive%20-%20Etec%20Centro%20Paula%20Souza/Desktop/Projetos/Sistema%20Digest%C3%B3rio_html/Instru%C3%A7%C3%B5es.html")
+}
+function abrirInstruções (){
+	open("file:///C:/Users/Pichau/OneDrive%20-%20Etec%20Centro%20Paula%20Souza/Desktop/Projetos/Sistema%20Digest%C3%B3rio_html/Instru%C3%A7%C3%B5es.html");
+	close("file:///C:/Users/Pichau/OneDrive%20-%20Etec%20Centro%20Paula%20Souza/Desktop/Projetos/Sistema%20Digest%C3%B3rio_html/Quiz.html")
+}
+function abrirCerto1(){
+	open("file:///C:/Users/Pichau/OneDrive%20-%20Etec%20Centro%20Paula%20Souza/Desktop/Projetos/Sistema%20Digest%C3%B3rio_html/P2.html");
+	close("file:///C:/Users/Pichau/OneDrive%20-%20Etec%20Centro%20Paula%20Souza/Desktop/Projetos/Sistema%20Digest%C3%B3rio_html/P1.html");
+}
+
+function abrirCerto2(){
+	open("file:///C:/Users/Pichau/OneDrive%20-%20Etec%20Centro%20Paula%20Souza/Desktop/Projetos/Sistema%20Digest%C3%B3rio_html/P3.html");
+	close("file:///C:/Users/Pichau/OneDrive%20-%20Etec%20Centro%20Paula%20Souza/Desktop/Projetos/Sistema%20Digest%C3%B3rio_html/P2.html");
+}
+function abrirCerto3(){
+	open("file:///C:/Users/Pichau/OneDrive%20-%20Etec%20Centro%20Paula%20Souza/Desktop/Projetos/Sistema%20Digest%C3%B3rio_html/P4.html");
+	close("file:///C:/Users/Pichau/OneDrive%20-%20Etec%20Centro%20Paula%20Souza/Desktop/Projetos/Sistema%20Digest%C3%B3rio_html/P3.html");
+}
+function abrirCerto4(){
+	open("file:///C:/Users/Pichau/OneDrive%20-%20Etec%20Centro%20Paula%20Souza/Desktop/Projetos/Sistema%20Digest%C3%B3rio_html/P5.html");
+	close("file:///C:/Users/Pichau/OneDrive%20-%20Etec%20Centro%20Paula%20Souza/Desktop/Projetos/Sistema%20Digest%C3%B3rio_html/P4.html");
+}
+function abrirCerto5(){
+	open("file:///C:/Users/Pichau/OneDrive%20-%20Etec%20Centro%20Paula%20Souza/Desktop/Projetos/Sistema%20Digest%C3%B3rio_html/P6.html");
+	close("file:///C:/Users/Pichau/OneDrive%20-%20Etec%20Centro%20Paula%20Souza/Desktop/Projetos/Sistema%20Digest%C3%B3rio_html/P5.html");
+}
+function abrirCerto6(){
+	open("file:///C:/Users/Pichau/OneDrive%20-%20Etec%20Centro%20Paula%20Souza/Desktop/Projetos/Sistema%20Digest%C3%B3rio_html/P7.html");
+	close("file:///C:/Users/Pichau/OneDrive%20-%20Etec%20Centro%20Paula%20Souza/Desktop/Projetos/Sistema%20Digest%C3%B3rio_html/P6.html");
+}
+function abrirCerto7(){
+	open("file:///C:/Users/Pichau/OneDrive%20-%20Etec%20Centro%20Paula%20Souza/Desktop/Projetos/Sistema%20Digest%C3%B3rio_html/P8.html");
+	close("file:///C:/Users/Pichau/OneDrive%20-%20Etec%20Centro%20Paula%20Souza/Desktop/Projetos/Sistema%20Digest%C3%B3rio_html/P7.html");
+}
+function abrirCerto8(){
+	open("file:///C:/Users/Pichau/OneDrive%20-%20Etec%20Centro%20Paula%20Souza/Desktop/Projetos/Sistema%20Digest%C3%B3rio_html/P9.html");
+	close("file:///C:/Users/Pichau/OneDrive%20-%20Etec%20Centro%20Paula%20Souza/Desktop/Projetos/Sistema%20Digest%C3%B3rio_html/P8.html");
+}
+function abrirCerto9(){
+	open("file:///C:/Users/Pichau/OneDrive%20-%20Etec%20Centro%20Paula%20Souza/Desktop/Projetos/Sistema%20Digest%C3%B3rio_html/P10.html");
+	close("file:///C:/Users/Pichau/OneDrive%20-%20Etec%20Centro%20Paula%20Souza/Desktop/Projetos/Sistema%20Digest%C3%B3rio_html/P9.html");
+}
+function abrirCerto10(){
+	open("file:///C:/Users/Pichau/OneDrive%20-%20Etec%20Centro%20Paula%20Souza/Desktop/Projetos/Sistema%20Digest%C3%B3rio_html/P11.html");
+	close("file:///C:/Users/Pichau/OneDrive%20-%20Etec%20Centro%20Paula%20Souza/Desktop/Projetos/Sistema%20Digest%C3%B3rio_html/P10.html");
+}
+function abrirCerto11(){
+	open("file:///C:/Users/Pichau/OneDrive%20-%20Etec%20Centro%20Paula%20Souza/Desktop/Projetos/Sistema%20Digest%C3%B3rio_html/P12.html");
+	close("file:///C:/Users/Pichau/OneDrive%20-%20Etec%20Centro%20Paula%20Souza/Desktop/Projetos/Sistema%20Digest%C3%B3rio_html/P11.html");
+}
+function abrirCerto12(){
+	open("file:///C:/Users/Pichau/OneDrive%20-%20Etec%20Centro%20Paula%20Souza/Desktop/Projetos/Sistema%20Digest%C3%B3rio_html/Parab%C3%A9ns.html");
+	close("file:///C:/Users/Pichau/OneDrive%20-%20Etec%20Centro%20Paula%20Souza/Desktop/Projetos/Sistema%20Digest%C3%B3rio_html/P12.html");
+}
+
+
+function img11(){
+	document.getElementById("trocarimg").src="img/boca_pulando.gif";	
+}
+function img12(){
+	document.getElementById("trocarimg").src="img/boca_falando.gif";	
+	alert("Bom dia jogador, vamos ver se você realmente prestou atenção no estudo sobre mim.");
+	alert("Tomara que sim, eu ia ficar meio triste se descobrisse que não, mas enfim.");
+	alert("Eu sou a boca aquela que você tanto usa todos os dias, que  dizem que adora falar sem parar, embora isso seja mentira… né?");
+	alert("Bom eu sei que sim, agora chega de papo furado.");
+}
+function img13(){
+	document.getElementById("trocarimg").src="img/boca_caindo.gif";	
+}
+
+
+
+function img21(){
+	document.getElementById("trocarimg").src="img/glandula-saindo.gif";	
+}
+function img22(){
+	alert("Bom dia jogador, eu sou…, quer dizer , nós somos as glândulas salivares e estamos aqui para testar seu conhecimento.");
+}
+function img23(){
+	document.getElementById("trocarimg").src="img/glandula-entrando.gif";	
+}
+
+
+function img31(){
+	document.getElementById("trocarimg").src="img/esofago-saindo.gif";	
+}
+function img32(){
+	alert("Olá jogador, me desculpe a aparência desgastada e exausta, mas fazer tanta força toda vez que você come me deixa meio cansado, mas vamos a pergunta.");
+}
+function img33(){
+	document.getElementById("trocarimg").src="img/esofago-entrando.gif";	
+}
+
+function img41(){
+	document.getElementById("trocarimg").src="img/figado-saindo.gif";	
+}
+function img42(){
+	alert("Olá,deixe-me me apresenta-me,  eu sou o figado, desculpa o atraso, eu estava conversando com a bile, então vamos para a pergunta! Qual minha principal função em seu corpo?");
+}
+function img43(){
+	document.getElementById("trocarimg").src="img/figado-entrando.gif";	
+}
+
+function img51(){
+	document.getElementById("trocarimg").src="img/estomago-saindo.gif";	
+}
+function img52(){
+	alert("Fala galera, creio que não preciso de uma introduções não é mesmo? de qualquer forma, eu sou o estomago, vamos ver se você estudou mesmo...Quais são os nomes das partes que me compõe?");
+}
+function img53(){
+	document.getElementById("trocarimg").src="img/estomago-entrando.gif";	
+}
+
+function img61(){
+	document.getElementById("trocarimg").src="img/vesicula-biliar-saindo.gif";	
+}
+function img62(){
+	alert("Me desculpa a demora estava armazenando a bile então vamos ver se você realmente aprendeu.");
+}
+function img63(){
+	document.getElementById("trocarimg").src="img/vesicula-biliar-entrando.gif";	
+}
+
+function img71(){
+	document.getElementById("trocarimg").src="img/pancreas.gif";	
+}
+function img72(){
+	alert("Oi jogador, vamos ver se você realmente estudou como deveria, ou as coisas vão ficar meio azedas pra você… eu não sou tão bom com frases de efeito.");
+}
+function img73(){
+	document.getElementById("trocarimg").src="img/pancreas-saindo.gif";	
+}
+
+function img81(){
+	document.getElementById("trocarimg").src="img/intestino-grosso-saindo.gif";	
+}
+function img82(){
+	alert("Opa, eu sou o intestino grosso, mas não quer dizer que não sou educado!");
+}
+function img83(){
+	document.getElementById("trocarimg").src="img/intestino-grosso-entrando.gif";	
+}
+
+function img91(){
+	document.getElementById("trocarimg").src="img/intestino-delgado-saindo.gif";	
+}
+function img92(){
+	alert("Olá eu sou o intestino delgado estava fazendo a minha função junto com minhas partes você sabe quais são elas?");
+}
+function img93(){
+	document.getElementById("trocarimg").src="img/intestino-delgado-entrando.gif";	
+}
+
+function img101(){
+	document.getElementById("trocarimg").src="img/intestino-delgado-saindo.gif";	
+}
+function img102(){
+	alert("Sim jogador pode achar que sou inútil, que sou apenas um enfeite fofo do seu corpo, mas eu também posso acabar te matando.");
+}
+function img103(){
+	document.getElementById("trocarimg").src="img/intestino-delgado-entrando.gif";	
+}
+
+function img121(){
+	document.getElementById("trocarimg").src="img/pancreas.gif";	
+}
+function img122(){
+	alert("Sem piadinhas… por favor.");
+}
+function img123(){
+	document.getElementById("trocarimg").src="img/pancreas-saindo.gif";	
+}
+
+function img111(){
+	document.getElementById("trocarimg").src="img/pancreas.gif";	
+}
+function img112(){
+	alert("Bom dia jogador, antes que você pergunte, sim sou eu que está fedendo, afinal tudo que seu corpo não quer passa por mim… todo dia..");
+}
+function img113(){
+	document.getElementById("trocarimg").src="img/pancreas-saindo.gif";	
+}
+
